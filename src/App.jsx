113b5714c8bb9b559cfafcb77965bbf55c9236c3
mahfuz091 <engmahfuz14@gmail.com>
2023-05-08 10:38:24 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useLoaderData } from "react-router-dom";
+import "./App.css";
+import CoffeeCard from "./components/CoffeeCard";
+
+function App() {
+  const coffees = useLoaderData();
+  return (
+    <>
+      <div className='m-20'>
+        <h1 className='text-6xl text-center my-20 text-purple-600'>
+          Hot Hot Cold Coffee: {coffees.length}
+        </h1>
+        <div className='grid md:grid-cols-2 gap-4'>
+          {coffees?.map((coffee) => (
+            <CoffeeCard
+              key={coffee._id}
+              coffee={coffee}
+              coffees={coffees}
+            ></CoffeeCard>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
